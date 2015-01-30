0fcb28d1094d8d98e72e305175b5081a535a9be8
John Guenin <john@guen.in>
2015-01-29 20:45:59 -0800

    Initial Commit

---
+++ LayoutPlusPlus-iOS/LayoutPlusPlus-iOS.h
@@ -0,0 +1,19 @@
+//
+//  LayoutPlusPlus-iOS.h
+//  LayoutPlusPlus-iOS
+//
+//  Created by John Guenin on 1/29/15.
+//  Copyright (c) 2015 John Guenin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LayoutPlusPlus-iOS.
+FOUNDATION_EXPORT double LayoutPlusPlus_iOSVersionNumber;
+
+//! Project version string for LayoutPlusPlus-iOS.
+FOUNDATION_EXPORT const unsigned char LayoutPlusPlus_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LayoutPlusPlus_iOS/PublicHeader.h>
+
+
